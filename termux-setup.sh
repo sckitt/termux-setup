a33b58a26bc45a778b3eab3d94ad44187409681c
sckitt <64489760@keemail.me>
2020-08-04 10:16:09 -0700

    updated packages

--- termux-setup.sh
+++ termux-setup.sh
@@ -3,7 +3,7 @@ cd ~
 # Update cache for package installs
 apt update && apt upgrade
 # grab all packages to install
-apt install cmatrix net-tools nmap python python2 -y
+pkg install cmatrix net-tools nmap python python2 wget -y
 pip install --upgrade youtube-dl pip
 # Setup storage
 termux-setup-storage
